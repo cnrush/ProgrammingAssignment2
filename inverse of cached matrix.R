8a18dc93d1e1c775e2006551e07d80b56d76c93c
cnrush <cnrush@live.com>
2016-03-14 02:40:12 -0400

    Added files via upload

--- inverse of cached matrix.R
+++ inverse of cached matrix.R
@@ -1,16 +1,22 @@
+#This code will first cache the inverse of an inputted SQUARE matrix. then call it.
+#first will calculate and cache
 makeCacheMatrix<-function(x = matrix()) {
+  #set some empty values and vectors to be populated later
   m <- NULL
   set<-function(y) {
     x<<-y
     m<<- NULL
   }
   get<-function() x
+  #solve is the function to obtain the inverse of the matrix here
+  #x is the matrix and m is now the inverse of it
   setInv<-function(solve) m<<-solve
   getInv<-function() m
   list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
 cacheSolve<-function(x, ...){
   m<-x$getInv()
+  #if inverse was calculated in makeCacheMatrix then it is not null
   if(!is.null(m)){
     message("getting cached data")
     return(m)
